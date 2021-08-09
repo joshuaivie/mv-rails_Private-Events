ede2c808f8b2f7c915ac12f1899804fa6b4bea66
Joshua Ivie <joshua@lifeonline.com.ng>
2021-08-09 22:10:41 +0100

    Add Bootstrap
    Signed-off-by: Joshua Ivie <joshua@lifeonline.com.ng>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,8 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+//= require jquery3
+//= require popper
+//= require bootstrap
 
+import * as bootstrap from 'bootstrap';
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,3 +11,27 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+document.addEventListener('turbolinks:load', () => {
+
+  let alertsList = [].slice.call(document.querySelectorAll('.alert-dismissible'))
+  let closeAlerts = alertsList.map((alert) => {
+    let dismissButton = document.createElement('button')
+    dismissButton.classList.add('btn-close')
+    dismissButton.setAttribute("type", "button");
+    dismissButton.setAttribute("aria-label", "Close");
+    dismissButton.setAttribute("data-bs-dismiss", "alert");
+    alert.appendChild(dismissButton)
+  })
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+})
