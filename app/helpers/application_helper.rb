d28dd4e46246dcb9c155496f995545599415f1c3
Joshua Ivie <joshua@lifeonline.com.ng>
2021-08-21 08:44:12 +0100

    Refactor Events List Item
    Signed-off-by: Joshua Ivie <joshua@lifeonline.com.ng>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ module ApplicationHelper
     if events.length.zero?
       render 'shared/no_event'
     else
-      render 'shared/events', events: events, event_state: options['event_state'], event_type: options['event_type']
+      render 'shared/events', events: events, event_state: options[:event_state],
+                              display_type: options[:display_type], profile: options[:profile]
     end
   end
 end
